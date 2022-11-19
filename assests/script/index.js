b7ce8263f47c6cfbb5c96817e23bd972fe8897aa
MrKunalmittal <kunalmittal19498@gmail.com>
2022-11-18 20:30:31 -0500

    Assests updated

--- assests/script/index.js
+++ assests/script/index.js
@@ -7,8 +7,8 @@
 -------------------------------------------------- */
 
 'use strict';
-function select(selector,parent=document){
-    return parent.querySelector(selector);
+function select(selector, parent = document) {
+  return parent.querySelector(selector);
 }
 
 let current = select('.clock');
@@ -21,46 +21,46 @@ const timeRegex = /^([01]?[0-9]|2[0-3]):[0-5][0-9]/;
 const alarmSound = new Audio('./assests/media/alarm.mp3');
 alarmSound.type = 'audio/mp3;'
 
-function currentTime () {
-    
-    let currentDate = new Date();
-    let hours = currentDate.getHours();
-    let minutes = currentDate.getMinutes();
-    let seconds = currentDate.getSeconds();
-    minutes =  minutes.toString().padStart(2, '0');
-    seconds = seconds.toString().padStart(2, '0');
-    
-    
-    //const timeString = (`${hours} : ${minutes} : ${seconds}`);
-    let timeString = hours + ':' + minutes + ':'+ seconds;
-    let alarmTime = hours + ':' + minutes;
+function currentTime() {
 
-    current.innerText = timeString;
-
-    let alarmValue = setAlarm.value.trim();
-    
-    
-    
-
-    alarm.addEventListener('click', function(){
-
-      if(alarmValue.length===0){
-        showAlarm.innerText ='Enter correct time\n';
-        valid=false;
-        
-      }else if(timeRegex.test(alarmValue)){
-        showAlarm.innerText = alarmValue;
-      }
-    });
-    
+  let currentDate = new Date();
+  let hours = currentDate.getHours();
+  let minutes = currentDate.getMinutes();
+  let seconds = currentDate.getSeconds();
+  minutes = minutes.toString().padStart(2, '0');
+  seconds = seconds.toString().padStart(2, '0');
 
 
-    if(alarmTime == alarmValue){
-      document.body.style.backgroundColor= 'red';
-      alarmSound.play();
-      
+  //const timeString = (`${hours} : ${minutes} : ${seconds}`);
+  let timeString = hours + ':' + minutes + ':' + seconds;
+  let alarmTime = hours + ':' + minutes;
+
+  current.innerText = timeString;
+
+  let alarmValue = setAlarm.value.trim();
+
+
+
+
+  alarm.addEventListener('click', function () {
+
+    if (alarmValue.length === 0) {
+      showAlarm.innerText = 'Enter correct time\n';
+      valid = false;
+
+    } else if (timeRegex.test(alarmValue)) {
+      showAlarm.innerText = alarmValue;
     }
-  };
-  
-  setInterval(currentTime, 1000);
+  });
+
+
+
+  if (alarmTime == alarmValue) {
+    document.body.style.backgroundColor = 'red';
+    alarmSound.play();
+
+  }
+};
+
+setInterval(currentTime, 1000);
 
