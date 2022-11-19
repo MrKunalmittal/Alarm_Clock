b6d3ba09d270b3e402bff5b41abd696b5ae021d0
MrKunalmittal <kunalmittal19498@gmail.com>
2022-11-18 19:01:32 -0500

    base clock created

---
+++ assests/script/index.js
@@ -0,0 +1,58 @@
+/*------------------------------------------------
+    object oriented js
+    Kunal mittal
+
+    Assignment 01
+    
+-------------------------------------------------- */
+
+'use strict';
+function select(selector,parent=document){
+    return parent.querySelector(selector);
+}
+
+let current = select('.clock');
+let showAlarm = select('.showalarm');
+let btn = select('.button');
+let alarm = select('.alarm');
+let setAlarm = select('.setalarm');
+
+const alarmSound = new Audio('./assests/media/alarm.mp3');
+alarmSound.type = 'audio/mp3;'
+
+function currentTime () {
+    
+    let currentDate = new Date();
+    let hours = currentDate.getHours();
+    let minutes = currentDate.getMinutes();
+    let seconds = currentDate.getSeconds();
+    minutes =  minutes.toString().padStart(2, '0');
+    seconds = seconds.toString().padStart(2, '0');
+    
+    
+    //const timeString = (`${hours} : ${minutes} : ${seconds}`);
+    let timeString = hours + ':' + minutes + ':'+ seconds;
+    let alarmTime = hours + ':' + minutes;
+
+    current.innerText = timeString;
+
+    let alarmValue = setAlarm.value;
+
+    alarm.addEventListener('click', function(){
+     
+      showAlarm.innerText = alarmValue;
+  
+    });
+    
+
+
+    if(alarmTime == alarmValue){
+      document.body.style.backgroundColor= 'lightgreen';
+      //document.alarm.style.backgroundColor = 'lightgreen';
+      alarmSound.play();
+      
+    }
+  };
+  
+  setInterval(currentTime, 1000);
+
